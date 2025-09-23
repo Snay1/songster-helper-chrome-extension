564cc9ab8d716c422899504bd22d517d613a85cd
Snay1 <89585275+Snay1@users.noreply.github.com>
2025-09-23 03:55:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,14 @@
 const initSongsterrScripts = () => {
     const initModalChecker = () => {
         const checker = () => {
-            const link = document.querySelector(".rq1ph a");
+            const link = document.querySelector(".C83v7g a");
             if (link) link.click();
         }
         setInterval(checker, 50);
 
         document.head.innerHTML += `
             <style type="text/css">
-                .rq25k, .Cip2pk {
+                .rq25k, .C8325s {
                     opacity: 0 !important;
                     display: none !important;
                     pointer-events: none !important;
