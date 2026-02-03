e926490d4b5bb4d9debd3a2899ab529b2f17eafd
Snay1 <stpshk767@gmail.com>
2026-02-04 02:17:43 +0300

    feat: modalChecker methods update

--- index.js
+++ index.js
@@ -2,18 +2,54 @@
 
 const initSongsterrScripts = () => {
     const initModalChecker = () => {
-        const checker = () => {
+
+        let awaitingPromise = false;
+
+        const checker = async () => {
             const link = document.querySelector(".C83v7g a");
-            if (link) link.click();
+            if (link && !awaitingPromise) {
+
+                awaitingPromise = true;
+
+                await new Promise((res) => {
+                    setTimeout(() => {
+                        res();
+                        link.click();
+                        awaitingPromise = false;
+                    }, 250);   
+                })
+
+            }
         }
-        setInterval(checker, 50);
+        setInterval(checker, 500);
 
         document.head.innerHTML += `
             <style type="text/css">
-                .rq25k, .C8325s {
+                .rq25k, 
+                .Cip2pk, 
+                #showroom, 
+                .C8325s, 
+                .uxw1uw, 
+                #showroom_header_desktop, 
+                #c-speed,
+                #c-loop,
+                #c-solo,
+                #c-mute,
+                #c-export,
+                #c-print,
+                .B3a4ku, 
+                .vgc1tt {
                     opacity: 0 !important;
                     display: none !important;
                     pointer-events: none !important;
+                }
+                // .Cfl4ni {
+                //     visibility: visible !important;   
+                // }
+
+                .B3a3b5 {
+                    justify-content: flex-start;
+                    gap: 30px;
                 }
             </style>
         `;
